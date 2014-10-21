3cc6653cd1804e805ef6518e0a9f4fbc0d1a05a2
Grzegorz Świrski <gswirski@computer.local>
2014-10-21 12:50:53 +0200

    Compile from minix

--- mshell.c
+++ mshell.c
@@ -48,22 +48,4 @@ main(int argc, char *argv[])
   }
 
   return 0;
-	line * ln;
-	command *com;
-
-	ln = parseline("ls -las | grep k | wc ; echo abc > f1 ;  cat < f2 ; echo abc >> f3\n");
-	printparsedline(ln);
-	printf("\n");
-	com = pickfirstcommand(ln);
-	printcommand(com,1);
-
-	ln = parseline("sleep 3 &");
-	printparsedline(ln);
-	printf("\n");
-
-	ln = parseline("echo  & abc >> f3\n");
-	printparsedline(ln);
-	printf("\n");
-	com = pickfirstcommand(ln);
-	printcommand(com,1);
 }
